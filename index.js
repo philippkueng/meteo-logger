3b9d64b2dbfdc2c04012480318218757c52b95a9
Philipp Küng <git@philippkueng.ch>
2015-02-16 20:49:42 +0100

    reading in the station code via CLI interface

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var fs = require('fs');
 var request = require('superagent');
 
-// make a sample request against the API
-request
-  .get('http://data.netcetera.com/smn/smn/BEZ')
-  .end(function(res) {
-    console.log(res.body);
+var getVersionNumber = function() {
+  return JSON.parse(fs.readFileSync('package.json')).version;
+};
+
+var fetchMeteoData = function(stationCode) {
+  request
+    .get('http://data.netcetera.com/smn/smn/' + stationCode)
+    .end(function(res) {
+      console.log(res.body);
+    });
+};
+
+/* CLI interface */
+
+program
+  .version(getVersionNumber());
+
+program
+  .command('fetch <station>')
+  .description('fetch data for the given MeteoSchweiz Station Code')
+  .action(function(station) {
+    fetchMeteoData(station);
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ ./index fetch BEZ');
+    console.log('    $ ./index fetch AIG');
+    console.log('');
   });
+
+program.parse(process.argv);
