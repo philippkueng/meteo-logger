27a22546d53a6423de835c2c0e389cef5da49c7c
Philipp Küng <git@philippkueng.ch>
2015-02-19 23:20:50 +0100

    cleaning up the functions to append the entries into a spreadsheet

--- index.js
+++ index.js
@@ -2,20 +2,56 @@
 
 var program = require('commander');
 var fs = require('fs');
+var async = require('async');
 var request = require('superagent');
 var spreadsheet = require('edit-google-spreadsheet');
 
-var getVersionNumber = function() {
+var getVersionNumber = function () {
   return JSON.parse(fs.readFileSync('package.json')).version;
 };
 
-var fetchMeteoData = function(stationCode) {
+var fetchMeteoData = function (stationCode, callback) {
   request
     .get('http://data.netcetera.com/smn/smn/' + stationCode)
     .end(function(res) {
-      console.log(res.body);
+      callback(null, res.body);
     });
 };
+
+var saveInSpreadsheet = function (data, callback) {
+  async.waterfall([
+      function (callback) {
+        callback(null, {
+          debug: true,
+          spreadsheetName: 'node-edit-spreadsheet',
+          worksheetName: 'Sheet1',
+          username: process.env.USERNAME,
+          password: process.env.PASSWORD
+        });
+      },
+
+      spreadsheet.load,
+
+      // get spreadsheet info
+      function (spreadsheet, callback) {
+        spreadsheet.receive(function (err, rows, info) {
+          callback(err, spreadsheet, info);
+        });
+      },
+
+      // save in spreadsheet
+      function (spreadsheet, info, callback) {
+        var line = {};
+        line[info.nextRow] = [[1, 2, 3, 4]];
+        spreadsheet.add(line);
+        spreadsheet.send(function (err) {
+          callback(err, 'done');
+        });
+      }
+    ], function (err, result) {
+      callback(null, result);
+    });
+}
 
 /* CLI interface */
 
@@ -25,9 +61,25 @@ program
 program
   .command('fetch <station>')
   .description('fetch data for the given MeteoSchweiz Station Code')
-  .action(function(station) {
-    fetchMeteoData(station);
-  }).on('--help', function() {
+  .action(function (station) {
+    async.waterfall([
+
+      // get the station code
+      function (callback) {
+        if (typeof station === 'string' && station.length === 3) {
+          callback(null, station);
+        } else {
+          callback(new Error('Invalid station code'), null);
+        }
+      },
+      fetchMeteoData,
+      convertToCsv,
+      saveInSpreadsheet
+      ], function (err, result) {
+        console.log(result);
+      });
+
+  }).on('--help', function () {
     console.log('  Examples:');
     console.log('');
     console.log('    $ ./index fetch BEZ');
